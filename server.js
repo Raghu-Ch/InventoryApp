54b3190d09a34169aae2eda301d05d4a4d4412e0
Raghu-Ch <raghu6004@gmail>
2018-09-11 11:31:15 -0400

    modified server file

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/ZID'));
+
+// app.get('/*', function (req, res) {
+
+//     res.sendFile(path.join(__dirname + '/dist/ZID/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
 const express = require('express');
+const http = require('http');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ZID'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/ZID/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/ZID/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || '6797';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
