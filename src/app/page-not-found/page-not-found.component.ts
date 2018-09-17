433c4b050fffe91bd5c1d68ec6a473332c4eb19f
Raghu-Ch <raghu6004@gmail>
2018-09-17 19:43:44 -0400

    fixed page-not-found c

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,16 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   template: `
-  <div class="center">
-    <mat-card fxFlex=50 fxLayout="column" class="center">
-      <i class="material-icons">error</i>
-      <p> 404! </p>
-      <p>
-        page-not-found!
-      </p>
-    </mat-card>
+  <div class="aligner" fxLayout="column" fxLayoutAlign="center center">
+    <i class="material-icons">error</i>
+    <p> 404! page-not-found</p>
   </div>
   `,
-  styles: ['p,i {color: red;}', 'i {font-size: 60px;}', '.center {align-items: center; justify-content: center;}']
+  styles: ['p,i {color: red; font-size: 24px;}', 'i {font-size: 90px;}', '.aligner {position: relative; top: 200px}']
 })
 export class PageNotFoundComponent { }
